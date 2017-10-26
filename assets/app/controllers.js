c7fb45290eb670ff2c91595c58db3e404c598a50
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2017-10-26 18:41:46 +0300

    Bugfixes and refactoring for new QS

--- assets/app/controllers.js
+++ assets/app/controllers.js
@@ -3,40 +3,43 @@ var API_PREFIX = 'https://json.smappi.org/adw0rd/chatea/';
 
 function MainCtrl ($http) {
     var vm = this;
-    vm.check_rooms = function(){
+    vm.context = {};
+    vm.checkRooms = function() {
         $http.get(API_PREFIX + 'getRooms').then(function (response) {
             vm.rooms = response.data;
         });
     }
-    vm.add_room = function(){
-        $http.get(API_PREFIX + 'addRoom', {roomname: vm.roomname}).then(function(response) {
+    vm.addRoom = function(){
+        $http.get(API_PREFIX + 'addRoom', {params: vm.context}).then(function (response) {
             if (response.data) {
-                vm.check_rooms()
+                vm.checkRooms();
             }
         });
     }
-    vm.check_rooms();
+    vm.checkRooms();
 }
 
 function RoomCtrl($http, $interval, $stateParams) {
     var vm = this;
+    vm.data = {}
     vm.roomname = $stateParams.name;
-    vm.get_data = function(){
-        $http.get(API_PREFIX + 'getRoom', {roomname: vm.roomname}).then(function (response) {
+    vm.context = {roomname: vm.roomname}
+    vm.getData = function () {
+        $http.get(API_PREFIX + 'getRoom', {params: {roomname: vm.roomname}}).then(function (response) {
             vm.data = response.data;
         });
     };
 
-    $interval(vm.get_data, 10000);
-    vm.get_data();
+    $interval(vm.getData, 10000);
+    vm.getData();
 
-    vm.add_comment = function() {
-        if (vm.message && vm.nickname && vm.data){
-            $http.get(API_PREFIX, {roomname: vm.roomname, nickname: vm.nickname, message: vm.message}).then(function(response) {
+    vm.addComment = function () {
+        if (vm.context.message && vm.context.nickname && vm.data.name) {
+            $http.get(API_PREFIX + 'addItem', {params: vm.context}).then(function (response) {
                 if (response.data) {
-                    vm.get_data()
-                    vm.message = "";
-                    $("#nickname").attr('disabled', true)
+                    vm.getData();
+                    vm.context.message = "";
+                    $("#nickname").attr('disabled', true);
                 }
             });
         }
